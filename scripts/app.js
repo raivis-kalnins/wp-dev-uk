02379bf8f7dadb2b52059bd1cebeb74443f1851e
raivis-kalnins <raivis@63.lv>
2019-10-23 13:39:26 +0100

    Mob menu
    Mob menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,27 @@
 var $ = jQuery.noConflict();
 function initialise() {
+    function mob_menu() {
+        var win = $(window);
+        if (win.width() < 1199) {
+            setTimeout(function(){
+                $('nav.main-menu-container').addClass('mob-menu');
+            }, 500);
+        } else {
+            $('nav.main-menu-container').removeClass('mob-menu');
+        }
+    }
+    mob_menu();
+    window.addEventListener("resize", function(){
+        mob_menu();
+    }, true);
+	$('body').on('click','#m-nav', function() {
+		$(this).addClass('clicked');
+		$(".mob-menu").addClass("on-toggle").css("visibility", "show");
+	});
+	$('body').on('click','#m-nav.clicked', function() {
+		$(this).removeClass('clicked');
+		$(".mob-menu").removeClass("on-toggle").css("visibility", "hide");
+	});
     $(".owl-carousel img").each(function(i, elem) {
         var img = $(elem);
         var div = $("<div />").css({"background-image": "url(" + img.attr("src") + ")"});
@@ -35,12 +57,12 @@ function initialise() {
     $("#calendar").datepicker({ firstDay: 1});
     $("img.logo-c").fadeIn(300);
     //$(".about-img img").parent().zoom();
-    $('section#sc2 .row div a').attr('href','#');
-
+    $('section#sc2 .row div a').attr('href','#');   
     TweenMax.defaultEase = Linear.easeOut;
     $('.main-menu-container ul').attr('id','MyMenu');
     $('#MyMenu li:nth-child(1)').addClass('active');
     var key = $('#f-key .textwidget').text();
+    $('#m-nav, .soc').removeClass('blue');
     //console.log('full key: '+ key);
     $('#fullpage').fullpage({
         licenseKey: key,
@@ -63,9 +85,11 @@ function initialise() {
                 $('#MyMenu li:nth-child(1)').addClass('active');
                 $('header').removeClass('n-fixed');
                 $('#fp-nav.fp-right').fadeOut();
+                $('#m-nav, .soc').removeClass('blue');
                 s_width = 0;
             } else if(destination.index == 1) {
                 $('#MyMenu li:nth-child(2)').addClass('active');
+                $('#m-nav, .soc').addClass('blue');
                 const about_img = $(".about-img");
                 const sec1 = document.querySelector("#sc1");
                 tl.fromTo(sec1, .7, { y: "0", opacity: 0 }, { y: "0", opacity: 1 });
@@ -73,22 +97,27 @@ function initialise() {
                 s_width = 16.6;
             } else if(destination.index == 2) {
                 $('#MyMenu li:nth-child(3)').addClass('active');
+                $('#m-nav, .soc').addClass('blue');
                 tl.fromTo(container, .7, { y: "0", opacity: 0 }, { y: "0", opacity: 1 });
                 s_width = 33.2;
             } else if(destination.index == 3) {
                 $('#MyMenu li:nth-child(4)').addClass('active');
+                $('#m-nav, .soc').addClass('blue');
                 tl.fromTo(container, .7, { y: "0", opacity: 0 }, { y: "0", opacity: 1 });
                 s_width = 49.8;
             } else if(destination.index == 4) {
                 $('#MyMenu li:nth-child(5)').addClass('active');
+                $('#m-nav, .soc').addClass('blue');
                 tl.fromTo(container, .7, { y: "0", opacity: 0 }, { y: "0", opacity: 1 });
                 s_width = 66.4;
             } else if(destination.index == 5) {
                 $('#MyMenu li:nth-child(6)').addClass('active');
+                $('#m-nav, .soc').addClass('blue');
                 tl.fromTo(container, .7, { y: "0", opacity: 0 }, { y: "0", opacity: 1 });
                 s_width = 83;
             } else if(destination.index == 6) {
                 $('#MyMenu li:nth-child(7)').addClass('active');
+                $('#m-nav, .soc').addClass('blue');
                 tl.fromTo(container, .7, { y: "0", opacity: 0 }, { y: "0", opacity: 1 });
                 s_width = 100;
             } else {
